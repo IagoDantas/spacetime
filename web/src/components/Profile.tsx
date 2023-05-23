afcc1cad6363a63c734ff8aebda98c6ec0a2d99d
Iago Dantas <iagodantas.8@gmail.com>
2023-05-23 07:05:53 -0300

    mobile - auth with github added

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -15,7 +15,10 @@ export function Profile() {
       />
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300 transition-colors">
+        <a
+          href=""
+          className="block text-red-400 transition-colors hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
