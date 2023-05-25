bb1fa10fc921a4ea8f65133145fbdfcadb92ce5b
Iago Dantas <iagodantas.8@gmail.com>
2023-05-25 02:33:59 -0300

    web - logout route and new memory page

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,6 +1,5 @@
 import { getUser } from '@/lib/auth'
 import Image from 'next/image'
-
 export function Profile() {
   const { name, avatarUrl } = getUser()
 
@@ -16,7 +15,7 @@ export function Profile() {
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
         <a
-          href=""
+          href="/api/auth/logout"
           className="block text-red-400 transition-colors hover:text-red-300"
         >
           Quero sair
